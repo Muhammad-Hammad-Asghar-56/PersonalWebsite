f89270cfedac247a6ea03cb1d7022227c1775c96
Muhammad Hammad Asghar <hammadasgharm@gmail.com>
2023-07-13 17:52:20 +0500

    remove frame-motion

--- src/Component/Notification.js
+++ src/Component/Notification.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { motion, AnimatePresence } from "framer-motion";
+
 
 const Notification = ({ messages }) => (
-  <AnimatePresence>
-    {messages.map(({ id, content }) => (
-      <motion.li
-        key={id}
-        initial={{ opacity: 0 }} // Added initial animation
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-      >
-        {content}
-      </motion.li>
-    ))}
-  </AnimatePresence>
+  // <AnimatePresence>
+  //   {messages.map(({ id, content }) => (
+  //     <motion.li
+  //       key={id}
+  //       initial={{ opacity: 0 }} // Added initial animation
+  //       animate={{ opacity: 1 }}
+  //       exit={{ opacity: 0 }}
+  //     >
+  //       {content}
+  //     </motion.li>
+  //   ))}
+  // </AnimatePresence>
+  <></>
 );
 
 export default Notification;
